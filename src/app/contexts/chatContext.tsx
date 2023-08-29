6b3fbf9b4fcad313f0ed73cc8512ef2b57c96165
Venzke <gabrielc.moreira97@gmail.com>
2023-08-28 22:48:20 -0300

    :tada: feat: commit inicial

---
+++ src/app/contexts/chatContext.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, createContext, useReducer } from "react";
+import { Message } from "../types/Message";
+import { chatReducer } from "../reducers/chatReduder";
+
+type ChatContext = {
+    chat: Message[];
+    addMessage: (user: string, text: string) => void;
+}
+
+export const  ChatContext = createContext<ChatContext | null>(null);
+
+export const ChatProvider = ({children} : {children: ReactNode}) => {
+    
+    const [chat, dispatch] = useReducer(chatReducer, []);
+
+    const addMessage = (user: string, text: string) => {
+        dispatch({
+            type: 'add',
+            payload: {user, text}
+        });
+    }
+
+    return (
+    <ChatContext.Provider value={{chat, addMessage}}>{children}</ChatContext.Provider>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { Chat } from "./components/Chat";
+import { UserProvider } from "./contexts/Usercontext";
+import { ChatProvider } from "./contexts/chatContext";
+
+const Page = (rating : string) => {
+
+  
+  return (
+
+    <div className="container mx-auto m-w-lg px-2">
+      <UserProvider>
+        <ChatProvider>
+          <h1 className="text-3xl my-3 text-center">Chat Simples</h1>
+
+          <Chat/>
+        </ChatProvider>
+      </UserProvider>
+    </div>
+  );
+
+}
+
+export default Page;
