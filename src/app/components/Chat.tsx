8d6cd9a6a94a75bdbdaa27e9d7cd8d7bdaac84a6
Venzke <gabrielc.moreira97@gmail.com>
2023-08-29 22:17:18 -0300

    :sparkles: feat: Criando component do nome

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,8 +1,12 @@
 import { useUser } from "@/app/contexts/Usercontext";
+import { NameInput } from "./NameInput";
 
 export const Chat = () => {
 
     const userCtx = useUser();
+
+    if (!userCtx) return null;
+    if (!userCtx.user) return <NameInput/>;
 
     return (
         <div className="border border-white/30 rounded-md text-3xl">
